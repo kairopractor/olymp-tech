6a2c99ad182ecf583a0fe81fa7571178e69395f3
Kai <kairopractor@yandex.ru>
2021-12-21 03:48:22 +0700

    make it better

--- config/connect.php
+++ config/connect.php
@@ -1,7 +1,20 @@
-<?php
+//<?php
 
-$connect = mysqli_connect(hostname: 'localhost', username: 'root', password: '', database: 'olimp');
+//$connect = mysqli_connect(hostname: 'localhost', username: 'root', password: '', database: 'olimp');
 
-if (!$connect) {
-    die('Error connect to database!');
-}
+//if (!$connect) {
+//    die('Error connect to database!');
+//}
+
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
