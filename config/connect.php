16e8285bd98072f25e5d0fc4f292d1b3bf4a6969
Kai <kairopractor@yandex.ru>
2021-12-24 04:30:12 +0700

    connect changes

--- config/connect.php
+++ config/connect.php
@@ -1,13 +1,13 @@
-//<?php
+<?php
 
-//$connect = mysqli_connect(hostname: 'localhost', username: 'root', password: '', database: 'olimp');
+/*$connect = mysqli_connect(hostname: 'localhost', username: 'root', password: '', database: 'olimp');
 
-//if (!$connect) {
-//    die('Error connect to database!');
-//}
+if (!$connect) {
+    die('Error connect to database!');
+}*/
 
 
-//Get Heroku ClearDB connection information
+Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
--- theme.php
+++ theme.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'config/connect.php';
+echo "<link rel='stylesheet' href='styles.css'>";
 
 ?>
 
@@ -10,75 +11,49 @@ require_once 'config/connect.php';
     <meta charset="UTF-8">
     <title>Themes</title>
 </head>
-<style>
-    th, td {
-        padding: 10px;
-    }
 
-    th {
-        background: #606060;
-        color: white;
-    }
-
-    td {
-        background: #b5b5b5;
-    }
-    h1 {
-        text-align: center;
-    }
-    img {
-        margin: 0 auto;
-        display: block;
-    }
-    form {
-        display: grid;
-        gap: 10px;
-        max-width: 400px;
-        margin: 0 auto;
-        padding-bottom: 40px;
-    }
-    label {
-        display: contents;
-    }
-</style>
 <body>
+
 <a href="index.php"><img src="https://nspu.ru/images/logo.png" width="150" /></a>
 
-    <h1> База данных ФГБОУ ВО "НГПУ"
-        <br />
-        "Олимпиада по технологии"
-    </h1>
-<table>
-    <tr>
-        <th>Номер</th>
-        <th>Название проекта</th>
-        <th>Ссылка на материал</th>
-        <th>Направление</th>
-    </tr>
+<h1> База данных ФГБОУ ВО "НГПУ"
+    <br />
+    "Олимпиада по технологии"
+</h1>
 
-    <?php
-    $themes = mysqli_query($connect, query: "SELECT * FROM `project`");
-    $themes = mysqli_fetch_all($themes);
-    for ($i = 0; $i < count($themes); $i++) {
-        $theme = $themes[$i];
-        ?>
-
+<div class="container">
+    <table>
         <tr>
-            <td><?= $i + 1 ?></td>
-            <td><?= $theme[1] ?></td>
-            <td><?= $theme[2] ?></td>
-            <td><?= $theme[3] ?></td>
-            <!-- <td><?= $theme[6] ?></td>
-            <td><?= $theme[7] ?></td>
-            <td><?= $theme[8] ?></td> -->
-            <!-- <td><a href="update.php?id=<?= $theme[0] ?>">Update</a></td> -->
-            <!-- <td><a style="color: crimson;" href="scripts/delete.php?id=<?= $theme[0] ?>">Delete</a></td> -->
+            <th>Номер</th>
+            <th>Название проекта</th>
+            <th>Ссылка на материал</th>
+            <th>Направление</th>
         </tr>
 
-        <?
-    }
-    ?>
-</table>
+        <?php
+        $themes = mysqli_query($connect, query: "SELECT * FROM `project`");
+        $themes = mysqli_fetch_all($themes);
+        for ($i = 0; $i < count($themes); $i++) {
+            $theme = $themes[$i];
+            ?>
+
+            <tr>
+                <td><?= $i + 1 ?></td>
+                <td><?= $theme[1] ?></td>
+                <td><?= $theme[2] ?></td>
+                <td><?= $theme[3] ?></td>
+                <!-- <td><?= $theme[6] ?></td>
+                <td><?= $theme[7] ?></td>
+                <td><?= $theme[8] ?></td> -->
+                <!-- <td><a href="update.php?id=<?= $theme[0] ?>">Update</a></td> -->
+                <!-- <td><a style="color: crimson;" href="scripts/delete.php?id=<?= $theme[0] ?>">Delete</a></td> -->
+            </tr>
+
+            <?
+        }
+        ?>
+    </table>
+</div>
 
 <form action="scripts/create.php" method="post">
     <h3>Add new mentor</h3>
